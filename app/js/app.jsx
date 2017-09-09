b23683f43986ed7894df660634ae541a00e251b7
Pablo Cerda <pablo@citla.com>
2017-09-10 00:28:40 +0200

    Initialization of git repository with experimental ReactJS application using slick component.

---
+++ app/js/app.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+var Slider = require('react-slick');
+var ReactDOM = require("react-dom");
+var ReactDriveIn = require("react-drive-in");
+
+class SimpleSlider extends React.Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <Slider>
+        <div><h3>1</h3></div>
+        <div><h3>2</h3></div>
+        <div><h3>3</h3></div>
+        <div><h3>4</h3></div>
+        <div><h3>5</h3></div>
+        <div><h3>6</h3></div>
+      </Slider>
+    );
+  }
+}
+
+ReactDOM.render(<SimpleSlider />, document.getElementById('carousel'));
